e145c8a6a19a183e86983ae1d5ebdcb44a8c0e16
chaudhari.chandan22@gmail.com <chaudhari.chandan22@gmail.com>
2024-03-30 01:17:34 +0530

    Second stage done

---
+++ src/logger.py
@@ -0,0 +1,6 @@
+import os  # Managing files and directories related to your dataset, model checkpoints, logs
+import sys # Accessing command-line arguments or controlling the execution environment of your script
+import logging #  Recording important information, warnings, errors, and other messages during the execution of your machine learning code 
+from datetime import datetime
+
+
