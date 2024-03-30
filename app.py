9082d8fc489133640f483b44be5de8f39575d013
chaudhari.chandan22@gmail.com <chaudhari.chandan22@gmail.com>
2024-03-30 15:16:34 +0530

    Second Commit Updated

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from src.logger import logging 
+
+app = Flask(__name__) 
+@app.route('/',methods=['GET','POST'])
+def index():
+    logging.info("Testing Logging Information")
+    return "Welcome to Machine Learning End to End Project"
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000,debug=True)
